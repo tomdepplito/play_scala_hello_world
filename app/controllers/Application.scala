e6a915eea89ac32ae337e91d32ea3937cdc7978d
Tom Depplito <tdepplito@jungroup.com>
2014-06-19 16:02:13 -0400

    Removed fib method from controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,15 +8,8 @@ object Application extends Controller {
 
   def index = Action {
     var testAnswer = new Answer(123, 6)
-    var answer:String = "Answer: " + testAnswer.result //fib(4)
+    println(testAnswer.result) // Test log to console
+    var answer:String = "Answer: " + testAnswer.result
     Ok(views.html.index(answer))
   }
-
-  def fib(num: Int): Int = {
-    if(num < 2)
-      return 1
-    else
-      fib(num-2) + fib(num-1);
-  }
-
 }
